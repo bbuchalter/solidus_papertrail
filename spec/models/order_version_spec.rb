dc642700c9320cc4f5b2026ecad9956449332268
Brian Buchalter <bal711@gmail.com>
2016-05-09 12:01:43 -0400

    Spec to turn off papertrail per model
    This demonstrates how to turn off Papertrail on a per model basis.

--- spec/models/order_version_spec.rb
+++ spec/models/order_version_spec.rb
@@ -10,4 +10,15 @@ describe Spree::OrderVersion do
       Spree::OrderVersion.count
     }.from(1).to(2)
   end
+
+  context "with paper trail turned off" do
+    before { Spree::Order.paper_trail_off! }
+    after { Spree::Order.paper_trail_on! }
+
+    it "does not create a version when an order is updated" do
+      expect {
+        order.update(email: 'test@test.com')
+      }.to_not change { Spree::OrderVersion.count }
+    end
+  end
 end
